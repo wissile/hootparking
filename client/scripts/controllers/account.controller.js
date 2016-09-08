eab62d92eb4c59eb30e71596bb6e9708f4560b26
sveltoz <coreteam@sveltoz.com>
2016-09-08 17:51:25 +0530

    facebook link changes in account.controller.js and account.html pages

--- client/scripts/controllers/account.controller.js
+++ client/scripts/controllers/account.controller.js
@@ -4,7 +4,7 @@ angular.module('easyparkangularApp')
   .controller('AccountCtrl', function ($scope, Auth, $location) {
 
       $scope.getCurrentUser = Auth.getCurrentUser();
-
+      $scope.appBackground = "#ede9e9";
       var date = $scope.getCurrentUser.dob;
       if (date) {
           var numbers = date.match(/\d+/g);
@@ -14,27 +14,29 @@ angular.module('easyparkangularApp')
       $scope.logout = function () {
           Auth.logout();
       };
-
-      
       $scope.SaveEditData = function (form) {
           $scope.submitted = true;
           var dob = new Date($scope.User.dob);
           var datalist = encodeURIComponent(JSON.stringify({ id: $scope.User._id, name: $scope.User.name, lastname: $scope.User.lastname, dob: dob, password: $scope.User.password, email: $scope.User.email }));
 
-          Auth.updateUser(datalist,function (data) {
+          Auth.updateUser(datalist, function (data) {
               // Logged in, redirect to home 
               $location.path('/account');
           });
       };
-
       $scope.fbLogin = function () {
-          Auth.fbLogin(function (data) {
+          Auth.fbLogin1(function (data) {
+              console.log(data);
               //Logged in, redirect to home 
               $location.path('/home');
           });
-      }
-      
+      };
 
+      $scope.UserOfFacebook = function () {
+          debugger;
+          console.log($scope.getCurrentUser.id);
+          window.open('https://www.facebook.com/' + $scope.getCurrentUser.id, '_system')
+      };
   });
 
 
