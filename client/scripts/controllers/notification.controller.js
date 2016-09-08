7821eef1248e40de64e3ddb073473f5b7b3759c1
sveltoz <coreteam@sveltoz.com>
2016-09-08 15:36:32 +0530

    notification page background

--- client/scripts/controllers/notification.controller.js
+++ client/scripts/controllers/notification.controller.js
@@ -3,6 +3,7 @@
 angular.module('easyparkangularApp')
     .controller('NotificationCtrl', function ($scope, Auth, $http) {
         $scope.Sweep = true;
+        $scope.appBackground = "#ede9e9";
         $scope.getCurrentUser = Auth.getCurrentUser();
         $scope.User = $scope.getCurrentUser;
         $http.get('/api/notification/' + $scope.User._id).success(function (data) {
@@ -14,7 +15,7 @@ angular.module('easyparkangularApp')
         });
 
         $scope.NotificationEvent = function (value, data) {
-             var datalist = encodeURIComponent(JSON.stringify({ value: value, data: data, userId: $scope.User._id }));
+            var datalist = encodeURIComponent(JSON.stringify({ value: value, data: data, userId: $scope.User._id }));
             $http.put('/api/notification/' + datalist).success(function (data) {
                 //return cb(); 
                 //  $scope.notificationSetting = data;
