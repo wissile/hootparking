3ea228c93eb94935d1c1ccfe5b21460b25c459e4
Sveltoz <coreteam@sveltoz.com>
2016-09-23 16:53:55 +0530

    Set spending budget and Set parking time reminder

--- client/scripts/controllers/notification.controller.js
+++ client/scripts/controllers/notification.controller.js
@@ -2,6 +2,9 @@
 
 angular.module('easyparkangularApp')
     .controller('NotificationCtrl', function ($scope, Auth, $http) {
+
+        debugger;
+        //   $scope.ParkingHrs = "notificationSetting.ParkingHrs";
         $scope.Sweep = true;
         $scope.appBackground = '#ede9e9';
         $scope.getCurrentUser = Auth.getCurrentUser();
@@ -9,13 +12,23 @@ angular.module('easyparkangularApp')
         $http.get('/api/notification/' + $scope.User._id).success(function (data) {
             //return cb(); 
             $scope.notificationSetting = data[0];
+            var ParkingTimeReminder = $scope.notificationSetting.ParkingTimeReminder;
+            var pt = ParkingTimeReminder.split(" : ");
+            $scope.ParkingHrs = pt[0];
+            $scope.ParkingMins = pt[1];
+            $scope.ParkingToggleBtnVal = pt[2];
+
+            debugger;
 
         }).error(function (err) {      // jshint ignore:line
             //return cb(err); 
         });
 
+        // $scope.ParkingHrs = $scope.notificationSetting.ParkingTimeReminder;
+
         $scope.ShowSpendingBudget = function () {
-            var abc = document.getElementById('SpendingBudget');
+            // debugger;
+            var abc = document.getElementById('DivSpendingBudget');
             if (abc.style.display !== 'none') {// jshint ignore:line
                 abc.style.display = 'none';
             }
@@ -26,7 +39,8 @@ angular.module('easyparkangularApp')
         }
 
         $scope.ShowReminder = function () {
-            var abc = document.getElementById('SpendingReminder');
+            //    debugger;
+            var abc = document.getElementById('DivParkingReminder');
             if (abc.style.display !== 'none') {// jshint ignore:line
                 abc.style.display = 'none';
             }
@@ -48,12 +62,64 @@ angular.module('easyparkangularApp')
             });
 
         };
-        
-        $scope.SaveParkingReminder = function () {
 
+        $scope.SaveSpendingBudget = function (SpendingBudget) {
+            debugger;
+            var spendingBudgetValue = document.getElementById('txtSpendingBudget').value;
+
+
+            var datalist = encodeURIComponent(JSON.stringify({ value: spendingBudgetValue, data: SpendingBudget, userId: $scope.User._id }));
+            $http.put('/api/notification/' + datalist).success(function (data) {      // jshint ignore:line
+                //return cb(); 
+                //  $scope.notificationSetting = data;
+
+            }).error(function (err) {     // jshint ignore:line
+                //return cb(err); 
+            });
 
         }
 
+        $scope.SaveParkingReminder = function () {
+            debugger;
+            var hrs = document.getElementById('txthrs').value;
+            var mins = document.getElementById('txtmins').value;
+            var toggleTime = document.getElementById('btnToggleTime').value;
+
+            var ParkingTimeReminder = hrs + ' : ' + mins + ' : ' + toggleTime;
+            var datalist = encodeURIComponent(JSON.stringify({ value: ParkingTimeReminder, data: 'ParkingTimeReminder', userId: $scope.User._id }));
+            $http.put('/api/notification/' + datalist).success(function (data) {      // jshint ignore:line
+                //return cb(); 
+                //  $scope.notificationSetting = data;
+
+            }).error(function (err) {     // jshint ignore:line
+                //return cb(err); 
+            });
+
+        }
+
+
+    })
+
+
+
+    .directive('startstop', function () {
+        return {
+            restrict: 'C',
+            replace: true,
+            template: '<input type="button" id="btnToggleTime" value="AM" ng-value="ParkingToggleBtnVal" />',
+            link: function (scope, elem, attrs) {
+                elem.bind("click", function () {
+                    console.log('startstop clicked', elem)
+                    if (elem.val() == "AM") {
+                        elem.val("PM");
+                    }
+                    else {
+                        elem.val("AM");
+                    }
+
+                })
+            }
+        }
     });
 
 
