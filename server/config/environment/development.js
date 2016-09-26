542d4a346180d7b1a2da20ab33d47c3fe6b38881
sveltoz <coreteam@sveltoz.com>
2016-09-26 12:13:26 +0530

    some changes

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -5,10 +5,10 @@
 module.exports = {
   // MongoDB connection options
     mongo: {
-        //uri: 'mongodb://202.88.154.118:27017/angularfullstack-dev'
+        //uri: 'mongodb://202.88.154.118/angularfullstack-dev'
 
-        uri: 'mongodb://localhost/angularfullstack-dev'
-  //  uri: 'mongodb://localhost/angularfullstack-dev'
+       // uri: 'mongodb://localhost/angularfullstack-dev'
+    uri: 'mongodb://localhost/angularfullstack-dev'
   },
 
   seedDB: true
